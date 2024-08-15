7116dbe2d57c92faa2780d75994e34d8de3ec302
jordanwu <89536016+WuChihWei@users.noreply.github.com>
2024-08-16 00:16:36 +0200

    merge top and introduction on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -41,12 +41,12 @@ const Navbar = () => {
       {isScrolled ? (
         <div className="introduction-carousel">
           <ul>
-            <li onClick={() => handleScrollToSection('introduction')} >Top</li>
-            <li onClick={() => handleScrollToSection('introduction')} >Introduction</li>
-            <li onClick={() => handleScrollToSection('user-story')}>User Story + Main Feature</li>
+            {/* <li onClick={() => handleScrollToSection('introduction')} >Top</li> */}
+            <li onClick={() => handleScrollToSection('introduction')} >Top / Introduction</li>
+            <li onClick={() => handleScrollToSection('user-story')}>User Story / Feature</li>
             <li onClick={() => handleScrollToSection('market-research')}>Market Research</li>
             <li onClick={() => handleScrollToSection('branding')}>Branding</li>
-            <li onClick={() => handleScrollToSection('wireframe-uis')}>Wireframe + Sketch</li>
+            <li onClick={() => handleScrollToSection('wireframe-uis')}>Wireframe / Sketch</li>
             <li onClick={() => handleScrollToSection('ui-guideline')}>UI Guideline</li>
             <li onClick={() => handleScrollToSection('tech')}>Tech</li>
             <li onClick={() => handleScrollToSection('user-testing')}>User Testing</li>
