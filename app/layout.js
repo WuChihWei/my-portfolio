799257e0c641f07f97533ab708d9dfb0b308dda9
jordanwu <89536016+WuChihWei@users.noreply.github.com>
2024-10-22 19:06:19 +0200

    add back description of superfake00

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
         {/* </ProjectsProvider> */}
         </main>
         <FooterWrapper />
+        {{ edit_1 }}  {/* Add the Analytics component here */}
       </body>
     </html>
   );
